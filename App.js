ffa5670faceefb331d258b7b65c732868fbbc9ad
OmarSaber1 <osaber218@gmail.com>
2023-10-08 01:55:12 +0300

    adjust styles

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputWrapper}>
+        <TextInput style={styles.inputStyle} placeholder='Enter Todo' />
+        <Button title='Add Todo' />
+      </View>
+      <View style={styles.listWrapper}>
+        <Text>List of Todos</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    padding: 50,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    flexDirection: "column",
+  },
+  inputWrapper: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
+    borderBottomColor: "skyblue",
+    borderBottomWidth: 2,
+  },
+  inputStyle: {
+    width: "70%",
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+  },
+  listWrapper: {
+    flex: 3,
+    marginTop: 20,
   },
 });
